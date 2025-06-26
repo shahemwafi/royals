a9da6a9cb97ef8b681faeaec0f468ee2f99bf023
Shahem Riaz <muhammadshahem@gmail.com>
2025-06-26 15:45:52 +0500

    Responsive

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // No additional JavaScript needed for now since Vanilla Tilt handles animations
 VanillaTilt.init(document.querySelectorAll("[data-tilt]"));
+
+  const hamburger = document.querySelector('.hamburger');
+  const sidebar = document.querySelector('.sidebar');
+
+  hamburger.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+  });
